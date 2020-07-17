dd95d94e5e3da762c93a99c2d647e8f4e32b5877
Maciej Pędzich <dnalop.me@gmail.com>
2020-07-17 20:52:23 +0200

    Add minimal value to budget

--- src/backend/models/category.js
+++ src/backend/models/category.js
@@ -2,7 +2,7 @@ const { model, Schema } = require('mongoose')
 
 const categorySchema = new Schema({
   name: { type: String, required: true },
-  budget: Number,
+  budget: { type: Number, min: 1 },
   expenses: [{
     type: Schema.Types.ObjectId,
     ref: 'Expense'
