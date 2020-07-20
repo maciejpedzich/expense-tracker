f28f79652a0e75971cd27abc6efbe7b79fdb3641
Maciej Pędzich <dnalop.me@gmail.com>
2020-07-20 11:43:00 +0200

    Add expiresAt field

--- src/backend/models/expense.js
+++ src/backend/models/expense.js
@@ -1,10 +1,12 @@
 const { model, Schema } = require('mongoose')
 
 const expenseSchema = new Schema({
-  title: { type: String, required: true },
+  name: { type: String, required: true },
   value: Number,
-  dateAdded: { type: Date, default: Date.now }
+  dateAdded: { type: Date, default: Date.now },
+  expiresAt: { type: Date, default: Date.now, expires: 1209600 }
 })
+
 const Expense = model('Expense', expenseSchema)
 
 module.exports = Expense
