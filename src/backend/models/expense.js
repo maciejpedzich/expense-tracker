296088de4f22cd7c24e5b39b0aa5a9cbcfd125c9
Maciej Pędzich <dnalop.me@gmail.com>
2020-07-24 21:24:11 +0200

    Remove "expiresAt" field

--- src/backend/models/expense.js
+++ src/backend/models/expense.js
@@ -3,8 +3,7 @@ const { model, Schema } = require('mongoose')
 const expenseSchema = new Schema({
   name: { type: String, required: true },
   value: Number,
-  dateAdded: { type: Date, default: Date.now },
-  expiresAt: { type: Date, default: Date.now, expires: 1209600 }
+  dateAdded: { type: Date, default: Date.now }
 })
 
 const Expense = model('Expense', expenseSchema)
